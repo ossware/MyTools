755b9ab22766b912d7de0526e695a62e70a4d3fb
haoxiaolei <xiaolei-0228@163.com>
2014-04-03 23:04:53 +0800

    测试修改

---
+++ WebApi/src/main/java/com/ehome/webapp/filter/CheckPermissionFilter.java
@@ -0,0 +1,29 @@
+package com.ehome.webapp.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @version V1.0
+ * @Project: MyTools
+ * @Title:
+ * @Package com.ehome.webapp.filter
+ * @Description: 权限验证过滤器
+ * @Author xiaolei-0228@163.com
+ * @Date 14-4-3 下午11:02
+ * @Copyright: 2014 ihome.com
+ */
+public class CheckPermissionFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
+}
