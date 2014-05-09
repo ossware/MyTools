b45f078dbf238808e63832797f9058e2bd8bce46
郝晓雷 <xiaolei-0228@163.com>
2014-05-09 20:07:41 +0800

    修改DruidUtil工具类的关闭方法

---
+++ BaiduYun/src/main/java/com/ehome/baidu/baidu.java
@@ -0,0 +1,14 @@
+package com.ehome.baidu;
+
+/**
+ * @version V1.0
+ * @Project: MyTools
+ * @Title:
+ * @Package com.ehome
+ * @Description:
+ * @Author xiaolei-0228@163.com
+ * @Date 2014/5/9 0009 19:59
+ * @Copyright: 2014 ihome.com
+ */
+public class baidu {
+}
